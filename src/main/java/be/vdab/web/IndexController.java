445572536e5d4058acbbf50a437ee5127956701a
Wouter Boghaert <Wouter Boghaert@192.168.0.104>
2018-01-11 17:53:19 +0100

    Oefening 12: Meertalig

--- src/main/java/be/vdab/web/IndexController.java
+++ src/main/java/be/vdab/web/IndexController.java
@@ -1,8 +1,11 @@
 package be.vdab.web;
 
+import java.time.LocalTime;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -10,7 +13,25 @@ class IndexController {
 	private static final String VIEW = "index";
 	
 	@GetMapping
-	String index() {
-		return VIEW;
+	ModelAndView index() {
+		String begroeting;
+		LocalTime nu = LocalTime.now();
+		if(nu.getHour() >= 0 && nu.getHour() <= 5) {
+			begroeting = "goede_nacht";
+		}
+		else {
+			if(nu.getHour() >=6 && nu.getHour() <= 11) {
+				begroeting = "goede_morgen";
+			}
+			else {
+				if(nu.getHour() >= 12 && nu.getHour() <= 17 ) {
+					begroeting = "goede_middag";
+				}
+				else {
+					begroeting = "goede_avond";
+				}
+			}
+		}
+		return new ModelAndView(VIEW, "begroeting", begroeting);
 	}
 }
